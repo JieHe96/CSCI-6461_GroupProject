bee946db24986291c91d115cf98f5afe19ca894c
Jack_He <hejie@usc.edu>
2018-09-12 17:00:10 -0400

    Add main function to GUI class

--- CSCI_6461_GroupProject/src/GUI.java
+++ CSCI_6461_GroupProject/src/GUI.java
@@ -1,4 +1,19 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.util.*;
 
-public class GUI {
-   fe
+public class GUI extends java.applet.Applet implements ActionListener
+{
+	public static void main (String[] args) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
