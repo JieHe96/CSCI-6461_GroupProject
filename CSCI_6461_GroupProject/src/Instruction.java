45c8de98ab69bcab0b5cbe46a96a7c420b133709
kitrak18 <43020982+kitrak18@users.noreply.github.com>
2018-09-14 11:39:37 -0400

    updated instruction class
    updated instruction class

--- CSCI_6461_GroupProject/src/Instruction.java
+++ CSCI_6461_GroupProject/src/Instruction.java
@@ -1,4 +1,23 @@
-//comment
-public class Instruction {
+/** 
+public class Instruction 
+{
+	// create objects 
+ 		opcode 
+ 		IX
+ 		R
+ 		I
+ 		Address
+ 		
+ 
+ 
+	//fetch input from the screen 
+	 * 
+	 * 
+	 */
 
+
+
+    // convert input into object values using switch case 
+
+	// 
 }
