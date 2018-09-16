8bc179cb8275dac75b286c71e8264d8bfb2c003b
Jack_He <hejie@usc.edu>
2018-09-16 12:20:50 -0400

    Initialize the Memory & Register class

--- CSCI_6461_GroupProject/src/Memory.java
+++ CSCI_6461_GroupProject/src/Memory.java
@@ -1,4 +1,19 @@
+import java.util.*;
 
 public class Memory {
-
+	
+	//private variable need for the memory class
+	private Vector<Integer> addrVec;
+	private Vector<Integer> memoVec;
+	private Register registers;
+	
+	public Memory() {
+		addrVec = new Vector<Integer> (32);
+		memoVec = new Vector<Integer> (2048);
+		registers = new Register();
+	}
+	
+	public Vector<Integer> GetAddr() {return addrVec;}
+	public Vector<Integer> GetMemo() {return memoVec;}
+	public Register GetRegister() { return registers; };
 }
