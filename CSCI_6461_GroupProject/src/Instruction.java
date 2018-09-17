189ef2edc870c8589d4364277d013fc5ea27f7cc
kitrak18 <43020982+kitrak18@users.noreply.github.com>
2018-09-17 14:38:16 -0400

    instruction class initialised

--- CSCI_6461_GroupProject/src/Instruction.java
+++ CSCI_6461_GroupProject/src/Instruction.java
@@ -1,7 +1,14 @@
-/** 
+/**Class that defines instruction objects  */ 
 public class Instruction 
 { 
-
+	
+	int opcode=0; // Initialised to zero, called for different values of opcodes 
+	int 
+	
+Instruction opcode=new Instruction();
+Instruction ix=new Instruction();
+Instruction r =new Instruction();
+Instruction =new Instruction();
 
 
 
