a5fbe995c72d38c8348c60a17bcda9c52777212d
kitrak18 <43020982+kitrak18@users.noreply.github.com>
2018-09-17 16:31:46 -0400

    decode

--- CSCI_6461_GroupProject/src/Decode.java
+++ CSCI_6461_GroupProject/src/Decode.java
@@ -1,5 +1,6 @@
 
-public class Decode<string> {
+public class Decode 
+{
     /**
      * fetch certain bits of a 16bits number
      * @param value 16bits data
@@ -10,26 +11,32 @@ public class Decode<string> {
 
     /**gettin the values from the instruction set */ 
 	
+	public int ToDecimal(String Str)
+    {
+        return ( Integer.parseInt(Str,2);
+    }
+    
+    public String ToBinary(Int X)
+    {
+        return (Integer.toBinaryString(X));
+    }
 
 
-    static private int getBit(int value, int start, int end) {
-        return value << (15-start) >>> (15-start+end);
+//    static private int getBit(int value, int start, int end) {
+  //      return value << (15-start) >>> (15-start+end);
     }
 
     //decode
 
-    public int ToDecimal(String Str)
-    {
-        return ( Integer.parseInt(Str,2);
-    }
+   /* 
 
     
-    public int iregister, iindex, itype, iaddress, hh;
+    //public int iregister, iindex, itype, iaddress, hh;
 
-    public void phaseDecode() throws Exception {
+//    public void phaseDecode() throws Exception {
 
         // Instruction register 
-        int ir = regs.getReg(regs.IR);
+   int ir = regs.getReg(regs.IR);
 
         // decode，op = ir[5:0] 
         //operand part of the instruction
@@ -69,15 +76,7 @@ public class Decode<string> {
         }
 
         //print("  ^ s=" + ss + " t=" + tt + " d=" + dd + " h=" + hh + " i=" + ii);
-    }
+    } */
 
   
-    public String ToBinary(Int X){
-        return (Integer.toBinaryString(X));
-        
-
-    }
-}
-
-}
-}
+   
