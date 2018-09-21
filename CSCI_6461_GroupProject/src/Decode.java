289ccde3ff2cb29b552ca86e436b3391d61d499b
Junwen Wen <wenjunwen@outlook.com>
2018-09-21 16:32:20 -0400

    DECODE

--- CSCI_6461_GroupProject/src/Decode.java
+++ CSCI_6461_GroupProject/src/Decode.java
@@ -1,28 +1,43 @@
 import java.util.Scanner;
 public class Decode 
 {
-    /**
-     * fetch certain bits of a 16bits number
-     * @param value 16bits data
-     * @param start high bit
-     * @param end	low bit
-     * @return		value of value[start:end]
-     */
 
-    /**gettin the values from the instruction set */ 
-	
+    /**
+     * converts string type-data to integer
+     *
+     * @param str
+     * @return integer of str
+     */
 	public static int ToDecimal(String str) {
         return (Integer.parseInt(str));
     }
-	
-	public static int binaryToDecimal(String str) {
+
+    /**
+     * converts binary to decimal
+     *
+     * @param str
+     * @return	decimal value of str
+     */
+    public static int binaryToDecimal(String str) {
 		return (Integer.parseInt(str, 2));
 	}
-    
+
+    /**
+     * converts a int data-type to 16bits binary string
+     *
+     * @param x
+     * @return	binary string of x
+     */
     public static String IntegerTo16sBinary(int x) {
     	return String.format("%16s", Integer.toBinaryString(x)).replace(" ", "0");
     }
-    
+
+    /**
+     * converts a int data-type to 12bits binary string
+     *
+     * @param x
+     * @return	binary string of x
+     */
     public static String IntegerTo12sBinary(int x) {
     	return String.format("%12s", Integer.toBinaryString(x)).replace(" ", "0");
     }
