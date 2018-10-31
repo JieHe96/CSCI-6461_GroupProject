65d1029f8eed8c318632aedf87adc58159e6033b
kitrak18 <43020982+kitrak18@users.noreply.github.com>
2018-10-31 17:38:42 -0400

    updates to fileloader

--- CSCI_6461_GroupProject/src/FileLoader.java
+++ CSCI_6461_GroupProject/src/FileLoader.java
@@ -23,6 +23,7 @@ public class FileLoader {
         int index= 350; // memory location where text chars are being stored
         int txt_len = 0; // length of the entire text
 		int ascii;
+		String element;
 		
 		while ((ascii = inputStream.read()) != -1) {
 			//System.out.println(ascii); // ascii is the ascii value and char(ascii) is the text. 
@@ -31,11 +32,16 @@ public class FileLoader {
 			// Printing to console
 			System.out.println(ascii + "\t" + inChar + "\t "+ inBin + "\t stored at  " + index + "\t " ); // pRinting to console
 			// writing to memory
-			MainApp.myMemory.writeToMemory(index, inBin);		// Storing to memory location 
+			MainApp.myMemory.writeToMemory(index, inBin);// Storing to memory location 
+			element=Character.toString(inChar);
+			MainApp.frame.setPrinter(element);
+			if(inChar=='.')
+				MainApp.frame.setPrinter("\n");
 			index++; // incrementing index for next storage
 			txt_len++;
 		}
-            
+		
+		MainApp.frame.setPrinter("\n  Enter search element in keyboard  \n ");// Storing to memory location
         System.out.print(" \t The total len" + txt_len);
 	}            
 } 
