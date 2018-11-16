2c7489351f24e8a0cbd2b804e0646d01ef6fc273
kitrak18 <43020982+kitrak18@users.noreply.github.com>
2018-11-16 15:24:04 -0500

    fpinstruction

--- CSCI_6461_GroupProject/src/FPInstruction.java
+++ CSCI_6461_GroupProject/src/FPInstruction.java
@@ -7,7 +7,8 @@ public class FPInstruction extends Instruction{
     private int instype;
     private int insadd;
     
-	public FPInstruction() {
+	// constructor 
+    public FPInstruction() {
     	super.value = new Word();
     	opcode = 0;
     	freg = 0;
@@ -15,4 +16,30 @@ public class FPInstruction extends Instruction{
     	instype = 0;
     	insadd = 0;
     }
+	
+    public void fetchInstruction() {
+		String ins = value.convertToString();
+		System.out.println(ins);
+		
+
+		String opbinary = ins.substring(0,6);
+		opcode = Decode.binaryToDecimal(opbinary);// decimal op
+		
+		if(opcode == 31)
+		{
+		
+		}
+		else 
+		{
+			String freg_bin = ins.substring(6,8);//r
+			String iindex = ins.substring(8,10);//ix
+			char temp = ins.charAt(10);
+			String itype = new StringBuilder().append("").append(temp).toString();
+			String iaddress = ins.substring(11,16);//address
+			
+		}
+		
+		
+    }
+	
 }
