f31b26be7df21debc57f4b83df4e7732cf996c8f
Jack_He <hejie@usc.edu>
2018-11-16 15:09:49 -0500

    Add constructor for FPinstruction

--- CSCI_6461_GroupProject/src/FPInstruction.java
+++ CSCI_6461_GroupProject/src/FPInstruction.java
@@ -1,4 +1,18 @@
 
 public class FPInstruction extends Instruction{
-
+	
+    private int opcode;
+    private int freg;
+    private int index;
+    private int instype;
+    private int insadd;
+    
+	public FPInstruction() {
+    	super.value = new Word();
+    	opcode = 0;
+    	freg = 0;
+    	index = 0;
+    	instype = 0;
+    	insadd = 0;
+    }
 }
