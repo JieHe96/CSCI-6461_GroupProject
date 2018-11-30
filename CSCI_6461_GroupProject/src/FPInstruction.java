13bc2df522ec8a2b507610030136d18f5da243f7
Jack_He <hejie@usc.edu>
2018-11-30 17:42:49 -0500

    update

--- CSCI_6461_GroupProject/src/FPInstruction.java
+++ CSCI_6461_GroupProject/src/FPInstruction.java
@@ -560,7 +560,7 @@ public class FPInstruction extends Instruction{
 					MainApp.myRegisters.writeToGR(ireg, res_fix);
 				}
 				else {
-					//fix to floating point
+					//fix to floating point num
 					String num_str = MainApp.myMemory.readFromMemory(convrt_ea).convertToString();
 					
 					char sign = '0';
